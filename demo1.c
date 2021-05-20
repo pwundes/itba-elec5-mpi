daf0462a38757def5747b79dfb5898a072c91c60
pwundes <pwundes@itba.edu.ar>
2021-05-20 00:31:28 +0000

    Simplified demo1.c

--- demo1.c
+++ demo1.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     int rank, size, i, provided;
     int A;
-    char hostname[50];
+    //char hostname[50];
 
     MPI_Init_thread(&argc, &argv, MPI_THREAD_SINGLE,&provided);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -14,18 +14,18 @@ int main(int argc, char *argv[])
 
     if (rank == 0) {
         for (i=1; i<size; i++)
-            MPI_Send(i, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&i, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
     } else {
-        MPI_Recv(A, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&A, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 
-    gethostname(&hostname);
+    //gethostname(&hostname);
 
     if (rank == 0)
     {
         printf("Esperando los mensajes...\n");
     } else {
-    	printf("My rank %d of %d, host: %s\n", rank, size, hostname );
+    	printf("My rank %d of %d\n", rank, size );
 	printf("A=");
         printf("%i ", A);
 	printf("\n");
